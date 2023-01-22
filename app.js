0b608fac161aae83b5b11b73f21ee87566b251d6
Mir Ruwayd Afeef <ruwaydafeef99@gmail.com>
2023-01-21 21:29:03 -0800

    more stuff

--- app.js
+++ app.js
@@ -1,3 +1,111 @@
+class UserAccount {
+  constructor(userName, password) {
+          this.firstName = null; //String
+          this.lastName = null; //String
+          this.userName = userName; //String
+          this.email = null; //String
+          this.phoneNumber = null; //String
+          this.password = password; //String
+          this.userDetails = null; //UserDetails object
+          this.team = null; //Team object
+    }
+
+    //accessor property(setter)
+  set setFirstName(firstName) {
+      this.firstName = firstName;
+  }
+
+  //accessor property(setter)
+  set setLastName(lastName) {
+      this.lastName = lastName;
+  }
+
+  //accessor property(setter)
+  set setEmail(email) {
+      this.email = email;
+  }
+
+  //accessor property(setter)
+  set setPhoneNumber(phoneNumber) {
+      this.phoneNumber = phoneNumber;
+  }
+
+  //accessor property(setter)
+  set setUserDetails(userDetails) {
+      this.userDetails = userDetails;
+  }
+
+  set setTeam(team) {
+      this.team = team;
+  }
+
+
+  }
+  
+    class Team {
+      constructor(teamName, teamDescription, userAccounts) {
+      this.id = generateString(5);
+      this.teamName = teamName; //String
+      this.teamDescription = teamDescription; //String
+      this.UserAccounts = userAccounts; //List of user accounts
+      this.teamLimit = 4;
+    }
+  }
+  
+  class UserDetails {
+      constructor(experienceLevel, languages, interests, 
+          hackathon, seriousness, checkboxGoals, stringGoals) {
+              this.experienceLevel = experienceLevel; //Experience level enum
+              this.languages = languages; //List of String
+              this.interests = interests; //List of String
+              this.hackathon = hackathon; //String
+              this.seriousness = seriousness; //int 1-10
+              this.checkboxGoals = checkboxGoals; //ListofGoals
+              this.stringGoals = stringGoals; //String
+              this.aboutMe = null; //String
+           }
+  }
+  
+  // Modifies: A given team and a User
+  // Requires: A team that is not full
+  // Effects: if has team returns true, prompt the user if they do indeed want to leave their team
+  
+  //modifies: both users teams as well as creates a new team
+  //requires: userAccounts that are both not on teams
+  //effects: constucts new team and places both users on said team
+  function createNewTeam(u1, u2) {
+      newTeamMembers = [u1, u2];
+      newTeam = new Team["DefaultName", "DefaultDescription", newTeamMembers];
+      u1.UserDetails.hasTeam = true;
+      u2.userDetails.hasTeam = false;
+      u1.team = newTeam;
+      u2.team = newTeam;
+  }
+
+  // Effects: If userAccount is on a team return true
+  function onTeam(u1) {
+      if (u1.team == nulll) {
+          return false
+      }
+      return true;
+  }
+  
+  
+  
+  
+  // make random string
+  const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+  function generateString(length) {
+      let result = ' ';
+      const charactersLength = characters.length;
+      for ( let i = 0; i < length; i++ ) {
+          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+  
+      return result;
+  }
+
 const bcrypt = require('bcrypt');
 const express = require('express');
 const path = require('path');
@@ -23,7 +131,8 @@ app.post('/sign-up', async (req, res) => {
   const coll = db.collection("User-sign_in");
 
   // Check if a similar username already exists in the collection
-  const existingUser = await coll.findOne({ username: req.body.username });
+  const usernameFinal = req.body.username;
+  const existingUser = await coll.findOne({ username: usernameFinal });
   if (existingUser) {
     res.sendFile(__dirname + "/sign-up-again.html")
     client.close();
@@ -40,10 +149,10 @@ app.post('/sign-up', async (req, res) => {
     password: hashedPassword
   } */
 
-  var newUser = new UserAccount(req.body.username, hashedPassword);
+  var newUser = new UserAccount(usernameFinal, hashedPassword);
   
   await coll.insertOne(newUser);
-  res.send("User Created!")
+  res.sendFile(__dirname + "/failure.html");
   client.close();
 });
 
@@ -74,5 +183,18 @@ app.post('/login', async (req, res) => {
 // Failure post
 app.post("/failure", function(req, res){
   res.redirect("/login");
+});
+//user-details post
+app.post('/user-details', async (req, res) => {
+  // Connect to the MongoDB cluster
+  const client = await mongodb.MongoClient.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+  const db = client.db("User_info");
+  const coll = db.collection("User-sign_in");
+
+  var newUser = new UserAccount(usernameFinal, hashedPassword,);
+  
+  await coll.insertOne(newUser);
+  res.sendFile(__dirname + "/failure.html");
+  client.close();
 });
 app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0' );
