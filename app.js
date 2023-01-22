7913b8c1733cc4f719d9cd0c3b7b377880f1645e
Mir Ruwayd Afeef <ruwaydafeef99@gmail.com>
2023-01-21 20:19:36 -0800

    Finished Sign up Page

--- app.js
+++ app.js
@@ -7,45 +7,69 @@ const connectionString = "mongodb+srv://ruwayd99:vBGjISx3ePtxYCG3@hackateam.nvut
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, 'public')));
-
-app.get("/", function(req, res){
-  res.sendFile(__dirname + "/sign-in.html");
+// sign up get
+app.get("/sign-up", function(req, res){
+  res.sendFile(__dirname + "/sign-up.html");
 });
-// Sign in post
-app.post('/', async (req, res) => {
+// login get 
+app.get("/login", function(req, res){
+  res.sendFile(__dirname + "/login.html");
+});
+// Sign up post
+app.post('/sign-up', async (req, res) => {
+  // Connect to the MongoDB cluster
+  const client = await mongodb.MongoClient.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+  const db = client.db("User_info");
+  const coll = db.collection("User-sign_in");
+
+  // Check if a similar username already exists in the collection
+  const existingUser = await coll.findOne({ username: req.body.username });
+  if (existingUser) {
+    res.sendFile(__dirname + "/sign-up-again.html")
+    client.close();
+    return;
+  }
   // Hash the password
   const saltRounds = 10;
   const password = req.body.password;
   const hashedPassword = await bcrypt.hash(password, saltRounds);
 
+  // Insert the new user in the collection
+  const newUser = {
+    username: req.body.username,
+    password: hashedPassword
+  }
+  await coll.insertOne(newUser);
+  res.send("User Created!")
+  client.close();
+});
+
+// Log-in post
+app.post('/login', async (req, res) => {
   // Connect to the MongoDB cluster
   const client = await mongodb.MongoClient.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
   const db = client.db("User_info");
   const coll = db.collection("User-sign_in");
-  
-  // insert the data in the collection
-  const data = {
-    username: req.body.username,
-    password: hashedPassword
+
+  // Find the user in the collection
+  const user = await coll.findOne({ username: req.body.username });
+  if(!user){
+    res.sendFile(__dirname + "/failure.html");
+    client.close();
+    return;
   }
-  await coll.insertOne(data);
+  // Compare the plaintext password with the hashed password stored in the MongoDB database
+  const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+
+  if (isPasswordCorrect) {
+    res.send('Logged in successfully');
+  } else {
+    res.sendFile(__dirname + "/failure.html");
+  }
   client.close();
-
-  res.send('Data received:\n');
 });
-
+// Failure post
+app.post("/failure", function(req, res){
+  res.redirect("/login");
+});
 app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0' );
-
-// app.post('/', async function (req, res) {
-//   try{
-//     let client = await mongodb.MongoClient.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
-//     let db = client.db("User_info");
-//     let coll = db.collection("User-sign_in");
-//     await coll.insertOne(req.body);
-//     client.close();
-//     res.send('Data received:\n' + JSON.stringify(req.body));
-//   } catch(err) {
-//     console.log(err);
-//     res.send(err);
-//   }
-// 
